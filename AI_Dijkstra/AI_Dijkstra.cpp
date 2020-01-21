e974f81a2065d120d887c74dddb58a8bfddc0ff8
Cameron_Murphy <cameronmurphyofficial@gmail.com>
2020-01-22 08:50:29 +1100

    Dijkstra shortest path algorithm

---
+++ AI_Dijkstra/AI_Dijkstra.cpp
@@ -0,0 +1,103 @@
+// AI_Dijkstra.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "dijkstrasSearch.h"
+
+
+
+int main()
+{
+	//structure looks like this
+	//		>N5--1-->N4
+	//	   /		   \
+	//    1			    1
+	//	 /				 \>
+	//	N1--10-->N2--10-->N3
+
+	//set up list of nodes
+	std::list<Pathfinding::Node*> nodes;
+
+	//set up node positions
+	Pathfinding::Node* node1 = new Pathfinding::Node();
+	node1->position.x = 0;
+	node1->position.y = 0;
+	
+	Pathfinding::Node* node2 = new Pathfinding::Node();
+	node2->position.x = 1;
+	node2->position.y = 0;
+	
+	Pathfinding::Node* node3 = new Pathfinding::Node();
+	node3->position.x = 2;
+	node3->position.y = 0;
+	
+	Pathfinding::Node* node4 = new Pathfinding::Node();
+	node4->position.x = 1.5;
+	node4->position.y = 1;
+	
+	Pathfinding::Node* node5 = new Pathfinding::Node();
+	node5->position.x = 0.5;
+	node5->position.y = 1;
+	
+
+	//set up edges
+
+	/*Pathfinding::Edge* edge1 = new Pathfinding::Edge(node2,10);
+	Pathfinding::Edge* edge2 = new Pathfinding::Edge(node3,10);
+	Pathfinding::Edge* edge3 = new Pathfinding::Edge(node3,1);
+	Pathfinding::Edge* edge4 = new Pathfinding::Edge(node4,1);
+	Pathfinding::Edge* edge5 = new Pathfinding::Edge(node5,1);*/
+
+	/*Pathfinding::Edge edge1;
+	edge1.target = node2;
+	edge1.cost = 10;
+	Pathfinding::Edge edge2;
+	edge2.target = node3;
+	edge2.cost = 10;
+	Pathfinding::Edge edge3;
+	edge3.target = node3;
+	edge3.cost = 1;
+	Pathfinding::Edge edge4;
+	edge4.target = node4;
+	edge4.cost = 1;
+	Pathfinding::Edge edge5;
+	edge5.target = node5;
+	edge5.cost = 1;*/
+
+	Edge edge1;
+	edge1.target = node2;
+	edge1.cost = 10;
+	Edge edge2;
+	edge2.target = node3;
+	edge2.cost = 10;
+	Edge edge3;
+	edge3.target = node3;
+	edge3.cost = 1;
+	Edge edge4;
+	edge4.target = node4;
+	edge4.cost = 1;
+	Edge edge5;
+	edge5.target = node5;
+	edge5.cost = 1;
+
+	
+	node1->connections.push_back(edge1);
+	node1->connections.push_back(edge5);
+	node2->connections.push_back(edge2);
+	node4->connections.push_back(edge3);
+	node5->connections.push_back(edge4);
+
+	nodes.push_back(node1);
+	nodes.push_back(node2);
+	nodes.push_back(node3);
+	nodes.push_back(node4);
+	nodes.push_back(node5);
+
+	dijkstrasSearch(node1, node4);
+
+	//clean up
+	for (auto node : nodes)
+	{
+		delete node;
+	}
+
+
+}
---
+++ AI_Dijkstra/dijkstrasSearch.h
@@ -0,0 +1,149 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include<list>
+
+struct Vector2
+{
+    Vector2() : x(0), y(0){}
+    Vector2(float X, float Y) : x(X), y(Y) {}
+    float x;
+    float y;
+};
+
+struct Edge;
+
+namespace Pathfinding
+{
+    
+    struct Node
+    {
+        Node() {}
+        ~Node() {}
+        // User defined data attached to each node.
+        Vector2 position;
+        float gScore;
+        Node* parent;
+        std::vector< Edge > connections;
+
+        bool operator <(Node* node)
+        {
+            if (node->gScore > gScore)
+            {
+                return true;
+            }
+            return false;
+        }
+    };
+
+}
+
+struct Edge
+{
+    Pathfinding::Node* target;
+    float cost;
+};
+
+
+std::list<Pathfinding::Node*> dijkstrasSearch(Pathfinding::Node* startNode, Pathfinding::Node* endNode)
+{
+    using namespace Pathfinding;
+    // Validate the input
+    if (startNode == NULL || endNode == NULL)
+    {
+        std::cout << "Error";
+        std::runtime_error;
+    }
+
+    if (startNode == endNode)
+    {
+        std::cout << "Error";
+        std::runtime_error;
+    }
+
+    // Initialise the starting node
+    startNode->gScore = 0;
+    startNode->parent = NULL;
+
+    // Create our temporary lists for storing nodes we’re visiting/visited
+    std::list<Node*> openList;
+    std::list<Node*> closedList;
+
+    openList.push_back(startNode);
+
+    Node* currentNode;
+
+    while (openList.size() != 0)
+    {
+        openList.sort();
+
+        currentNode = openList.front();
+        // If we visit the endNode, then we can exit early.
+        // Sorting the openList guarantees the shortest path is found,
+       // given no negative costs (a prerequisite of the algorithm).
+        if (currentNode == endNode)
+        {
+            break;
+        }
+
+        openList.remove(currentNode);
+        closedList.push_back(currentNode);
+
+        int gScore = 0;
+        bool nodeFoundCL = false; //CL stands for closed list
+        bool nodeFoundOL = false; //OL stands for openList
+        for (Edge c : currentNode->connections)
+        {
+            for (Node* node : closedList)//is this target in the closedList
+            {
+                if (node == c.target)
+                {
+                    nodeFoundCL == true; //yes it was found
+                }
+            }
+            if (!nodeFoundCL)
+            {
+                gScore = currentNode->gScore + c.cost;
+
+                // Have not yet visited the node.
+                // So calculate the Score and update its parent.
+                // Also add it to the openList for processing.
+                for (Node* node : openList)//is this target in the openList
+                {
+                    if (node == c.target)
+                    {
+                        nodeFoundOL = true; //yes it was found
+                    }
+                }
+                if (!nodeFoundOL)
+                {
+                    c.target->gScore = gScore;
+                    c.target->parent = currentNode;
+                    openList.push_back(c.target);
+                }
+                // Node is already in the openList with a valid Score.
+                // So compare the calculated Score with the existing
+                // to find the shorter path.
+                else if (gScore < c.target->gScore)
+                {
+                    c.target->gScore = gScore;
+                    c.target->parent = currentNode;
+                }
+            }
+
+        }
+
+    }
+    // Create Path in reverse from endNode to startNode 
+    std::list<Node*> path;
+    currentNode = endNode;
+
+    while (currentNode != NULL)
+    {
+        path.push_front(currentNode);
+        currentNode = currentNode->parent;
+    }
+    // Return the path for navigation between startNode/endNode 
+    return path;
+}
